0bd856abd1e83dffd48d5c34de7ba5a9cc11b0bd
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-06-06 12:44:39 -0500

    added port to server for heroku

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
 
 const app = express();
 
